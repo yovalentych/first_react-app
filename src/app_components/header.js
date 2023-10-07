bf973c9b9b5147e8d0ad3c708cd3b52c9a16b5bc
yovalentych <jovalentych@gmail.com>
2023-10-07 09:44:03 +0300

    first commit

---
+++ src/app_components/header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <a href="#">HOME</a>
+        <a href="#">Contacts</a>
+        <a href="#">About</a>
+        <a href="#">Info</a>
+      </header>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/app_components/technologies.js
@@ -0,0 +1,35 @@
+function Technologies() {
+  return (
+    <>
+      <h1>Technologies</h1>
+      <div className="App">
+        <div>
+          <h1>Front End</h1>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JS</li>
+          </ul>
+        </div>
+        <div>
+          <h1>Back End</h1>
+          <ul>
+            <li>Node.js</li>
+            <li>Python</li>
+            <li>PHP</li>
+          </ul>
+        </div>
+        <div>
+          <h1>BareBack</h1>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JS</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Technologies;
